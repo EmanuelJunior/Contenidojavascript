7a2bed8b6f0ceec6b2de1a887b56e6572013a4e1
Emanuel Garces <sagazu13@gmail.com>
2020-12-07 15:57:36 -0500

    Capitulos 14 y 15 actualizados correctamente

--- 14 Metodos de cadena Javascript/codigo.js
+++ 14 Metodos de cadena Javascript/codigo.js
@@ -1,10 +1,10 @@
 /*METODOS DE CADENA*/
 
 /*
-Concat() -> este metodo lo que hace es juntar una o mas
+NombreCadena.concat() -> este metodo lo que hace es juntar una o mas
 cadenas devolviendo una cadena nueva combinada con las demas.
 
-startsWith() -> este metodo lo que hace es verificar si el comienzo
+NombreCadena.startsWith() -> este metodo lo que hace es verificar si el comienzo
 de una cadena es igual a otra cadena EJEMPLO 
 
 cadena = "Emanuel es un joven";
@@ -23,14 +23,14 @@ de la primera cadena hay un espacio lo cual en la cadena 2
 no hay ese espacio, puede er que haya la misma palabra pero
 este metodo verifica todo.
 
-endsWith() -> este metodo hace lo mismo que startWith la diferencia
+NombreCadena.endsWith() -> este metodo hace lo mismo que startWith la diferencia
 es que en vez de verificar el comienzo de dos cadenas lo que hace
 es verifica la ultima palabra de dos cadenas verificando si son
 iguales o no EJEMPLO
 
 cadena_Verificar.endsWith(cadena_Comparar);
 
-includes() -> Este metodo no verfica si al comienzo a el final dos cadenas
+NombreCadena.includes() -> Este metodo no verfica si al comienzo a el final dos cadenas
 son identicas, este metodo lo que hace es verificar si en las dos cadenas
 hay una concidencia de palabras y si existe esa concidencia devolvera true,
 de lo contrario false.
@@ -45,7 +45,7 @@ resultado = cadena1.includes(cadena2); -> devolvera true porque en la cadena 2 h
 con la cadena que se esta verificando que es la primera debido a que exactamente la cadena2 tiene
 coincidencia con la cadena1.
 
-indexOf() -> este metodo lo unico que hace es indicar en que numero de letra se encuentra la palabra
+NombreCadena.indexOf() -> este metodo lo unico que hace es indicar en que numero de letra se encuentra la palabra
 con la que estamos buscando coincidencias, es decir sirve exactamente para saber en que letra esta la 
 palabra que se esta buscando este indica la posicion en conteo pero del comienzo de la primera palabra.
 EJEMPLO
@@ -61,11 +61,11 @@ Hay que tener en cuenta que esta va a mostrar la primer coincidencia que haya en
 si tenemos una cadena que dice varias veces (buenas) lo que pasara es que mostrara la posicion de la primera
 palabra que se encuentre en dicha cadena.
 
-lastIndexOf() -> Este metodo en vez de mostrar la posicion de la primera palabra que se este buscando lo que 
+NombreCadena.lastIndexOf() -> Este metodo en vez de mostrar la posicion de la primera palabra que se este buscando lo que 
 hara es mostrar la posicion de la palabra que se este buscando pero la ultima palabra que hay en toda la cadena
 con esa coincidencia.
 
-padStart() -> El método padStart() rellena la cadena actual con una cadena dada (repetida eventualmente) de modo
+NombreCadena.padStart() -> El método padStart() rellena la cadena actual con una cadena dada (repetida eventualmente) de modo
 que la cadena resultante alcance una longitud dada. El relleno es aplicado desde el inicio (izquierda) de la cadena
 actual. EJEMPLO
 
@@ -77,7 +77,7 @@ El primer dato que hay que darle es la longitud que queremos que tenga la cadena
 cadena con la que queremos rellenar, hay que tener en cuenta que esta cadena a agregar, se agregara al principio osea 
 izquierda en la cadena actual
 
-padEnd() -> El padEnd() método rellena la cadena actual con una cadena determinada (repetida, si es necesario) para que la cadena
+NombreCadena.padEnd() -> El padEnd() método rellena la cadena actual con una cadena determinada (repetida, si es necesario) para que la cadena
 resultante alcance una longitud determinada. El relleno se aplica desde el final de la cadena actual. EJEMPLO
 
 cadena1 = "BuenosDias";
@@ -91,14 +91,14 @@ Es decir tanto padStart() como padEnd() rellenan una cadena ya sea al comienzo o
 que le pasemos este complete puede ser que este acortada o que se repita eso dependera de cuanto indiquemos el tamaño que tendra nuestr
 cadena.
 
-repeat() -> Este metodo tiene la funcionalidad de repetir cadenas las veces que nosotros le indiquemos. EJEMPLO
+NombreCadena.repeat() -> Este metodo tiene la funcionalidad de repetir cadenas las veces que nosotros le indiquemos. EJEMPLO
 
 cadena1 = "1234 ";
 
 resultado = cadena1.repeat(3); -> El resultado sera el siguiente: (1234 1234 1234) debido a que le indicamos que se repitiera
 la cadena 3 veces.
 
-split() -> Es un metodo que tiene como funcionalidad convertir una cadena en un array que esta separado de acuerdo a un indicador
+NombreCadena.split() -> Es un metodo que tiene como funcionalidad convertir una cadena en un array que esta separado de acuerdo a un indicador
 que elijamos para separar dicha cadena en diferentes partes. Lo que va en comillas es el separador que indicaremos para separar nuestra
 cadena y convertir nuestra cadaena en un array EJEMPLO
 
@@ -113,7 +113,7 @@ que se separaran por espacios lo que indica que cada espacio creara un indice co
 
 document.write(resultado[1]) -> Esto devolvera Noches porque el indice 1 de el array es Noches.
 
-substring() -> Este metodo es como el metodo assoc() en php la funcionalidad que tiene es devolver una posicion inicial y otra final de una
+NombreCadena.substring() -> Este metodo es como el metodo assoc() en php la funcionalidad que tiene es devolver una posicion inicial y otra final de una
 cadena, por ejemplo escogemos 0 y la posicion 10 esto lo que hara es mostrarnos en la cadena el contenido que tiene desde la posicion 0 hasta
 la posicion 10 de la cadena EJEMPLO
 
@@ -123,42 +123,42 @@ resultado = cadena1.substring(0, 8) -> Esto no devolvera -> (Buenos d) debido a 
 En pocas palabras la primera parte del metodo substring() es el inicio y la segunda es el final lo que indica lo que nos va a mostrar 
 exactamente de la cadena.
 
-toLowerCase() -> Este metodo lo que hace es convertir una cadena a letra minuscula EJEMPLO
+NombreCadena.toLowerCase() -> Este metodo lo que hace es convertir una cadena a letra minuscula EJEMPLO
 
 cadena1 = "Buenas tardes";
 resultado = cadena1.toLowerCase(); -> buenas tardes
 
-toUpperCase() -> Este metodo lo que hace es convertir una cadena a letra mayuscula EJEMPLO
+NombreCadena.toUpperCase() -> Este metodo lo que hace es convertir una cadena a letra mayuscula EJEMPLO
 
 cadena1 = "Buenas tardes";
 resultado = cadena1.toUpperCase(); -> BUENAS TARDES
 
-toString() -> Este metodo lo que permite hacer es convertir cualquier variable en un string. EJEMPLO
+NombreCadena.toString() -> Este metodo lo que permite hacer es convertir cualquier variable en un string. EJEMPLO
 
 variable = 12550;
 resultado = variable.toString(); -> "12550";  
 
-trim() -> Este metodo lo que hace es eliminar los espacios tanto de la derecha como de la izquierda sobrantes
+NombreCadena.trim() -> Este metodo lo que hace es eliminar los espacios tanto de la derecha como de la izquierda sobrantes
 en una cadena de texto -> string. EJEMPLO
 
 cadena1  = "           Hola         ";
 resultado = cadena1.trim(); -> "Hola"; 
 
-trimStart() -> Este metodo lo que hace es eliminar los espacios de una cadena de texto pero al principio 
+NombreCadena.trimStart() -> Este metodo lo que hace es eliminar los espacios de una cadena de texto pero al principio 
 EJEMPLO
 
 cadena1  = "           Hola         ";
 resultado = cadena1.trimStart(); -> "Hola         "; -> Esto devolveria debido a que la funcionalidad es eliminar espacios
 sobrantes pero al principio de la cadena.
 
-trimEnd() -> Este metodo lo que hace es eliminar los espacios de una cadena de texto pero al final 
+NombreCadena.trimEnd() -> Este metodo lo que hace es eliminar los espacios de una cadena de texto pero al final 
 EJEMPLO
 
 cadena1  = "           Hola         ";
 resultado = cadena1.trimEnd(); -> "           Hola"; -> Esto devolveria debido a que la funcionalidad es eliminar espacios
 sobrantes pero al final de la cadena.
 
-.length -> Esta es una propiedad de las cadenas de texto tiene como funcionalidad devolver el tamaño de una cadena 
+NombreCadena.length -> Esta es una propiedad de las cadenas de texto tiene como funcionalidad devolver el tamaño de una cadena 
 de texto. EN NUMEROS. EJEMPLO
 
 cadena1 = "Hola como esta";
@@ -347,6 +347,24 @@ cadena = "                   EspaciosVaciosQuitadosAlFinal              ";
 resultado = cadena.trimEnd();
 
 document.write('<hr>'+resultado+'<hr>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 15 Metodos de array Javascript/codigo.js
+++ 15 Metodos de array Javascript/codigo.js
@@ -6,7 +6,7 @@ de nuestro array creado. EJEMPLO
 
 let frutas = ["Banano","Naranja","Guanabana"];
 let resultado = frutas.pop(); -> Esto lo que hara es borrar el elemento
-de nuestro array llamado -> "Guanabana" 
+de nuestro array llamado -> "Guanabana"
 
 Osea que si miramos lo que tiene ya nuestro array de frutas seria unicamente
 el indice 0 => "Banano" y el indice 1 => "Naranja"
@@ -16,18 +16,18 @@ de nuestro array creado. EJEMPLO
 
 let frutas = ["Banano","Naranja","Guanabana"];
 let resultado = frutas.shift(); -> Esto lo que hara es borrar el elemento
-de nuestro array llamado -> "Banano" 
+de nuestro array llamado -> "Banano"
 
 Osea que si miramos lo que tiene ya nuestro array de frutas seria unicamente
 el indice 1 => "Naranja" y el indice 2 => "Guanabana"
 
 NombreArray.push() -> Este metodo la funcionalidad que tiene es la de agregar
 mas elementos a un array, dentro de .push("NombreElemento","NombreDeOtroElemento")
-y de esta manera podemos agregar mas elementos a un array cada elemento a agregar 
+y de esta manera podemos agregar mas elementos a un array cada elemento a agregar
 puede estar separado por coma para identificar de que es otro elemento del array. EJEMPLO
 
 let frutas = ["Banano","Naranja","Guanabana"];
-let resultado = frutas.push("Sandia", "Mango") 
+let resultado = frutas.push("Sandia", "Mango")
 
 Ahora lo que tendra este array sera
 => frutas = ["Banano","Naranja","Guanabana","Sandia", "Mango"]
@@ -38,16 +38,16 @@ NombreArray.reverse() -> Este metodo la funcionalidad que tiene es invertir
 el orden de los elementos de un array. EJEMPLO
 
 let numeros = [1, 2, 3, 4, 5];
-numeros.reverse(); -> Lo que devolvera sera lo siguiente -> 
+numeros.reverse(); -> Lo que devolvera sera lo siguiente ->
 numeros = [5, 4, 3, 2, 1];
 
 NombreArray.unshift() -> Este metodo es parecido a .push() debido a que su funcionalidad
-tambien es agregar elementos al array pero en vez de agregar elementos al array al final, 
+tambien es agregar elementos al array pero en vez de agregar elementos al array al final,
 este lo que hace es agregar elementos arl array pero al principio, osea que todos los elementos
 que agregemos empezaran desde la posicion 0.
 
 let frutas = ["Banano","Naranja","Guanabana"];
-let resultado = frutas.unshift("Sandia", "Mango") 
+let resultado = frutas.unshift("Sandia", "Mango")
 
 Ahora lo que tendra este array sera
 => frutas = ["Sandia", "Mango","Banano","Naranja","Guanabana"]
@@ -72,7 +72,7 @@ numeros = [3,5,8,2,4];
 PARA ELIMINAR:
 
 resultado = numeros.splice(INDICE COMIENZO, INDIE HASTA DONDE ELIMINAR);
-resultado = numeros.splice(3, 4); Esto lo que haria es eliminar los elementos 
+resultado = numeros.splice(3, 4); Esto lo que haria es eliminar los elementos
 del array desde la posicion 3 hasta la 4 por lo que quedaria de esta manera el array
 
 numeros = [3, 5, 8] -> Los que se eliminaron fueron el 2 => 3 y el elemento 4 => 4
@@ -82,14 +82,94 @@ PARA AGREGAR:
 resultado = numeros.splice(1, 0, 23, 45, 56);
 
 Aca lo que indicamos es que empezara a agregar los elementos desde la posicion 1 , despues
-ponemos 0 que indica que son elementos indefinidos los que vamos a agregar y por ultimo van 
+ponemos 0 que indica que son elementos indefinidos los que vamos a agregar y por ultimo van
 los elementos que estan separados por comas y son cada uno de los elementos a agregar.
 
 numeros = [3, 23, 45, 56, 5, 8, 2, 4];
 
+NombreArray.join("Seperador de elementos") -> Este metodo funciona para convertir de array a cadena
+de texto, este es diferente a toString() debido a que este le puedes indicar el separador de cada indice
+del array EJEMPLO
 
+numeros = [2, 3, 5, 7, 1, 8];
+resultado = numeros.join(" - ");
+
+Si nosotros mostramos por pantalla la cadena de texto nos devolvera lo siguiente
+RESULTADO: 2 - 3 - 5 - 7 - 1 - 8
+
+Asi logramos decir que convirtiera el array en una cadena ademas de que cada elemento va a estar
+separado por un guion, nosotros podemos poner cualquier separacion para los elementos del array por ejemplo
+pódemos utilizar <br> o cualquier etiqueta para hacer una separacion entre cada elemento del que era el array.
+
+NombreArray.slice() -> Este metodo funciona para mostrar ciertos elementos de el array que nosotros indiquemos
+a traves del indice, el primer parametro es el primer indice que queremos que se muestre y el segundo parametro
+es hasta que indice queremos que se muestre los elementos, este ultimo parametro el indice que pongamos no estara
+incluido los anteriores a este si. EJEMPLO
+
+nombres = ["Emanuel","Santiago","Oscar","Veronica"];
+resultado = nombres.slice(1, 3);
+
+Esto lo que nos mostrara es los elementos del indice 1 y el 2, el 3 no porque ese numero no esta incluido
+
+Si queremos mostrar todos los elementos del array pero no sabemos hasta que indice va, lo que podemos hacer es ponre
+el primer parametro que indique desde donde va a mostrar y no poner el segundo parametro eso indicara que muestre todos
+desde la posicion inical dada hasta mostrar todos los elementos.
+
+Los metodos de cadena de texto toString(), indexOf(), lastIndexOf() y includes() -> Estos metodos funcionan de igual manera
+para los arrays ya que cumplen la misma funcionalidad la diferencia es que si queremos buscar algun elemento o saber si existe
+tendremos que poner el texto completamente para que identifique el indice en donde se encuentra o si en verdad existe en el array.
+
+NombreArray.filter(nombreAponer) -> Este metodo y el metodo .forEach() este funciona para recorrer un array y asi mismo crear una funcion
+dentro de este metodo hay que tener en cuenta que esta funcion debe tener un parametro, ese parametro va hacer el recorrido de cada uno
+de los elementos del array en pocas palabras cuando escribamos ese parametro nos mostrara cada uno de los elementos del array, es
+practicamente un ciclo. EJEMPLO
+
+nombres = ["Emanuel","Santiago","Oscar","Veronica"];
+
+Forma 1 -> funcion flecha UNA LINEA
+
+nombres.filter((nombre) => document.write(`<hr>${nombre}</hr>`));
+nombres.forEach((nombre) => document.write(`<hr>${nombre}</hr>`));
+
+Forma 2 -> funcion flecha
+
+nombres.filter((nombre)=>{
+document.write(`<hr>${nombre}</hr>`);
+document.write('<hr>');
+});
+
+nombres.forEach((nombre)=>{
+document.write(`<hr>${nombre}</hr>`);
+document.write('<hr>');
+});
+
+Forma 3 -> funcion
+
+nombres.filter(function(nombre){
+document.write(`<hr>${nombre}</hr>`);
+document.write('<hr>');
+});
+
+nombres.forEach(function(nombre){
+document.write(`<hr>${nombre}</hr>`);
+document.write('<hr>');
+});
+
+El metodo forEach() y filter() son casi identicos la unica diferencia entre estos dos es que en el metodo filter()
+podemos crear una variable que contenga logica con filter eso lo que nos permitira que al llamar dicha variable solo
+se muestre los elementos que cumplan dichas condiciones ejemplo.
+
+resultado = nombres.filter(nombre=> nombre.length > 5);
+
+document.write('<hr><br>'+resultado);
+
+Aca estamos creando una variable que recorre el array de nombre y que solo al mostrar la variable resultado mostrara unicamente
+los elementos que tengan un tamaño de mas de 5 caracteres, aca podemos poner cualquier tipo de condicion y unicamente se mostraran
+los elementos que la cumplan por esta razon es mejor utilizar filter en vez de forEach() debido a que filter() es mucho mas completo
+y tiene muchas mas posibilidades.
 
 */
+
 
 /*METODO POP*/
 
@@ -172,6 +252,69 @@ document.write(`Nombres eliminados de posicion 3 a la 5: <b>${nombres}</b><hr>`)
 
 nombres = ["Emanuel","Santiago","Oscar","Juan","Ester","Mariana"];
 
-Resultado = nombres.splice(1, 0, "Robert","Ezequiel");
+resultado = nombres.splice(1, 0, "Robert","Ezequiel");
 
 document.write(`Nombres Agregados desde la posicion 5: <b>${nombres}</b><hr>`);
+
+/*METODO JOIN*/
+
+nombres = ["Paulina","Valentina","Jessica","Mike","Louis","Veronica"];
+
+resultado = nombres.join("<hr><b>Nombre:</b> ");
+
+document.write(`<hr><b>Nombre:</b> ${resultado}`);
+
+/*METODO SLICE*/
+
+nombres = ["Paulina","Valentina","Jessica","Mike","Louis","Veronica"];
+
+resultado = nombres.slice(1);
+
+document.write('<hr>'+resultado+'<hr');
+
+/*METODO FILTER*/
+
+nombres = ["Paulina","Valentina","Jessica","Mike","Louis","Veronica"];
+
+/*FORMA 1*/
+
+nombres.filter(nombre => document.write('<br><hr>'+nombre+'<br>'));
+
+/*FORMA 2*/
+
+nombres.filter(nombre =>{
+document.write(`<br><br><hr>Nombres: <b>${nombre}</b>`);
+})
+
+/*FORMA 3 */
+
+nombres.filter(function(nombre){
+document.write(`<br><br><hr>Nombres: <b style='color:green'>${nombre}</b>`);
+})
+
+/*FORMA 4 CON LOGICA*/
+
+resultado = nombres.filter(nombre=> nombre.length > 5);
+
+document.write('<hr><br>'+resultado);
+
+/*METODO FOREACH*/
+
+nombres = ["Paulina","Valentina","Jessica","Mike","Louis","Veronica"];
+
+/*FORMA 1*/
+
+nombres.forEach(nombre=>document.write(`<hr><br>${nombre}<br></hr>`));
+
+/*FORMA 2*/
+
+nombres.forEach(nombre =>{
+document.write(`<br><br><hr>Nombres: <b>${nombre}</b>`);
+})
+
+/*FORMA 3 */
+
+nombres.forEach(function(nombre){
+document.write(`<br><br><hr>Nombres: <b style='color:purple'>${nombre}</b>`);
+})
+
